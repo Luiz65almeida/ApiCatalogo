424b16e8dd3275b4fa7dc08877d783b3aa8b687e
Luiz Henrique Almeida <luizhenrique65almeida@gmail.com>
2024-07-09 16:07:04 -0300

    Povoando o banco de dados

---
+++ ApiCatalogo/Migrations/20240709175758_PopulaProduto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos (Nome, Descricao, preco, ImagemUrl, estoque, DataCadastro, CategoriaId)" + "Values ('cola cola Zero', 'Refrigerante de cola 350ml', 5.54, 'cocacola.jpg', 50, now(), 1)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from produtos");
+        }
+    }
+}
